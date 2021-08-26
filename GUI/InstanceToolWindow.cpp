0f89893069de5f92ec81601ff8e55af266bd50ce
Sam <itsthatguyagain3@gmail.com>
2021-08-26 19:07:00 +1000

    Instances window now has better layout <3

--- GUI/InstanceToolWindow.cpp
+++ GUI/InstanceToolWindow.cpp
@@ -11,46 +11,60 @@ void InstanceToolWindow::Draw()
         if (ImGui::Button("Find Instances") && GS::currentClass) {
             GS::instances.clear();
             GS::instances = FindAllInstances((uintptr_t)GS::currentClass->VTable);
+            if (GS::instances.size() > 128) {
+                GS::instances = std::vector<uintptr_t>(GS::instances.begin(), GS::instances.begin() + 128);
+            }
         }
         if (GS::instances.size() != 0) {
+            ImGui::Separator();
             ImGui::Text("Found %d instances in memory", GS::instances.size());
-            for (unsigned int i = 0; i < GS::instances.size(); i++) {
-                bool badVtable = false;
-                if (GS::currentClass) {
-                    if (!IsBadReadPointer((void*)GS::instances[i])) {
-                        if (*(uintptr_t*)GS::instances[i] != ((uintptr_t)GS::currentClass->VTable ^ 0xDEADBEEF))
-                        {
+            if (ImGui::BeginChild("MemInstances", {WindowSize[0], 350}))
+            {
+                ImGui::Separator();
+                for (unsigned int i = 0; i < GS::instances.size(); i++) {
+                    bool badVtable = false;
+                    if (GS::currentClass) {
+                        if (!IsBadReadPointer((void*)GS::instances[i])) {
+                            if (*(uintptr_t*)GS::instances[i] != ((uintptr_t)GS::currentClass->VTable ^ 0xDEADBEEF))
+                            {
+                                ImGui::TextColored({ 255,0,0,1 }, POINTER_FMTSTRING, GS::instances[i]);
+                                if (ImGui::IsItemClicked()) {
+                                    GS::CopyToClipboard(POINTER_FMTSTRING, GS::instances[i]);
+                                }
+                                badVtable = true;
+                            }
+                        }
+                        else {
                             ImGui::TextColored({ 255,0,0,1 }, POINTER_FMTSTRING, GS::instances[i]);
                             if (ImGui::IsItemClicked()) {
                                 GS::CopyToClipboard(POINTER_FMTSTRING, GS::instances[i]);
+
                             }
                             badVtable = true;
                         }
                     }
-                    else {
-                        ImGui::TextColored({ 255,0,0,1 }, POINTER_FMTSTRING, GS::instances[i]);
+                    if (!badVtable) {
+                        ImGui::TextColored({ 0,255,0,1 }, POINTER_FMTSTRING, GS::instances[i]);
                         if (ImGui::IsItemClicked()) {
                             GS::CopyToClipboard(POINTER_FMTSTRING, GS::instances[i]);
-
                         }
-                        badVtable = true;
                     }
                 }
-                if (!badVtable) {
-                    ImGui::TextColored({ 0,255,0,1 }, POINTER_FMTSTRING, GS::instances[i]);
-                    if (ImGui::IsItemClicked()) {
-                        GS::CopyToClipboard(POINTER_FMTSTRING, GS::instances[i]);
-                    }
-                }
+                ImGui::EndChild();
             }
         }
         if (GS::codeReferences.size() != 0) {
+            ImGui::Separator();
             ImGui::Text("Found %d vtable references in code", GS::codeReferences.size());
-            for (unsigned int i = 0; i < GS::codeReferences.size(); i++) {
-                ImGui::TextColored({ 0,255,225,1 }, POINTER_FMTSTRING, GS::codeReferences[i]);
-                if (ImGui::IsItemClicked()) {
-                    GS::CopyToClipboard(POINTER_FMTSTRING, GS::codeReferences[i]);
+            ImGui::Separator();
+            if (ImGui::BeginChild("CodeRef")) {
+                for (unsigned int i = 0; i < GS::codeReferences.size(); i++) {
+                    ImGui::TextColored({ 0,255,225,1 }, POINTER_FMTSTRING, GS::codeReferences[i]);
+                    if (ImGui::IsItemClicked()) {
+                        GS::CopyToClipboard(POINTER_FMTSTRING, GS::codeReferences[i]);
+                    }
                 }
+                ImGui::EndChild();
             }
         }
         ImGui::SetWindowPos(WindowPos);
